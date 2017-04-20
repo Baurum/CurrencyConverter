d62549230f31768434fd69de6e6ce1e9c2dce558
Marcos <baurumtec@gmail.com>
2017-04-20 12:28:27 +0200

    Add funcionality to convert from different currency

--- app/src/main/java/bau/com/converter/MainActivity.java
+++ app/src/main/java/bau/com/converter/MainActivity.java
@@ -1,11 +1,16 @@
 package bau.com.converter;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -23,15 +28,28 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private final int USD = 0;
+    private final int PLN = 1;
+    private final int GBP = 2;
+    private final int EUR = 3;
+
+
     public double usd;
     public double pln;
     public double gbp;
+    public double eur;
     public double userValue;
     public String userMoney;
     public EditText etUserMoney;
     public TextView tvUsd;
     public TextView tvPln;
     public TextView tvGbp;
+    public TextView tvEur;
+    Spinner spCurrencyUser;
+    public Context mContext;
+    public int selectedCurrency;
+
+
 
 
 
@@ -39,32 +57,146 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initApp();
+
     }
     /***********************************************************************************************
      * Method to start.
      **********************************************************************************************/
     private void initApp(){
+        mContext = this;
         etUserMoney = (EditText) findViewById(R.id.et_user_money);
         tvUsd = (TextView) findViewById(R.id.tv_usd);
         tvPln = (TextView) findViewById(R.id.tv_pln);
         tvGbp = (TextView) findViewById(R.id.tv_gbp);
+        tvEur = (TextView) findViewById(R.id.tv_eur);
+        this.spCurrencyUser = (Spinner)    findViewById(R.id.spinner_currency);
+
+        loadUserCurrency();
+
 
     }
 
+    /***********************************************************************************************
+     * Method array adapter
+     **********************************************************************************************/
+    private void loadUserCurrency(){
+
+        final ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource
+                        (this, R.array.user_currency, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spCurrencyUser.setAdapter(adapter);
+        this.spCurrencyUser.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (parent.getId()){
+                    case R.id.spinner_currency:
+                        String[]  arrayUserCurrency = getResources().getStringArray(R.array.user_currency);
+                        ArrayAdapter<CharSequence> adapter =
+                                new ArrayAdapter<CharSequence>
+                                        (mContext, android.R.layout.simple_spinner_item,
+                                                android.R.id.text1, arrayUserCurrency);
+                        adapter.setDropDownViewResource
+                                (android.R.layout.simple_spinner_dropdown_item);
+                        InputMethodManager imm =
+                                (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(spCurrencyUser.getWindowToken(), 0);
+
+                        //store selected currency info
+                        selectedCurrency = position;
+
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    /**
+     *
+     * @param view
+     */
     public void getData(View view){
-        userValue = Double.parseDouble(etUserMoney.getText().toString());
-        new DownloadCurrencyTask().execute();
+
+        if(etUserMoney.getText().toString().equals("")){
+            etUserMoney.setError("topo mete un numero");
+        }else {
+            userValue = Double.parseDouble(etUserMoney.getText().toString());
+            new DownloadCurrencyTask().execute();
+            refreshCurrencyFields();
+        }
+    }
+
+    /**
+     * method
+     */
+    public void refreshCurrencyFields(){
+        tvEur.setVisibility(View.VISIBLE);
+        tvUsd.setVisibility(View.VISIBLE);
+        tvPln.setVisibility(View.VISIBLE);
+        tvGbp.setVisibility(View.VISIBLE);
+        switch (selectedCurrency){
+            case EUR:
+                tvEur.setVisibility(View.GONE);
+                break;
+
+            case USD:
+                tvUsd.setVisibility(View.GONE);
+                break;
+
+            case GBP:
+                tvGbp.setVisibility(View.GONE);
+                break;
+
+            case PLN:
+                tvPln.setVisibility(View.GONE);
+                break;
+
+            default:
+                break;
+
+        }
 
     }
+
+
+    /**
+     *
+     */
     private class DownloadCurrencyTask extends AsyncTask<Void, Void, String> {
+        private String getUrl(){
+            String result = "https://api.fixer.io/latest?";
+            switch (selectedCurrency){
+                case EUR:
+                    result += "symbols=USD,PLN,GBP&base=EUR";
+                    break;
+                case USD:
+                    result += "symbols=EUR,PLN,GBP&base=USD";
+                    break;
+                case PLN:
+                    result += "symbols=USD,EUR,GBP&base=PLN";
+                    break;
+                case GBP:
+                    result += "symbols=USD,PLN,EUR&base=GBP";
+            }
+            return result;
+        }
+
+
         protected String doInBackground(Void... params) {
             String result = "";
             InputStream inputStream = null;
-
             URL url = null;
             try {
-                url = new URL("https://api.fixer.io/latest");
+                url = new URL(getUrl());
                 // create HttpURLConnection
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -94,20 +226,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        /**
+         *
+         * @param progress
+         */
         protected void onProgressUpdate(Integer... progress) {
 //            etProgressPercent(progress[0]);
         }
 
+        /**
+         *
+         * @param result
+         */
         protected void onPostExecute(String result) {
 
             Log.d(TAG, result);
             try{
                 JSONObject data = new JSONObject(result);
                 JSONObject rates = data.getJSONObject("rates");
-                usd = rates.getDouble("USD");
-                pln = rates.getDouble("PLN");
-                gbp = rates.getDouble("GBP");
-                Log.d(TAG, String.valueOf(usd));
+                if(rates.has("USD")) usd = rates.getDouble("USD");
+                if(rates.has("PLN"))   pln = rates.getDouble("PLN");
+                if(rates.has("GBP")) gbp = rates.getDouble("GBP");
+                if(rates.has("EUR"))  eur = rates.getDouble("EUR");
+
+//                Log.d(TAG, String.valueOf(usd));
 
 
 ////                String base = data.getString("base");
@@ -122,7 +264,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     *
+     */
     private void multiplyCurrency(){
+
+        tvEur.setText("EUR " + String.valueOf(userValue * eur));
 
         tvUsd.setText("USD: " + String.valueOf(userValue * usd));
 
